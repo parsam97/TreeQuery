9c8d89d2622acf50e9f4772debebe8a4fd571f57
Parsa Motamedi <parsamotamedi@gmail.com>
2024-11-19 18:03:59 +0100

    running ps1

--- main.ps1
+++ main.ps1
@@ -1,21 +1,12 @@
 using module .\SOQLBuilder.psm1
 
+$ExcludedFields = @('OwnerId')
+$FieldSelector = { ($_.createable -eq $true -and $_.updateable -eq $true) -and $_.name -notin $ExcludedFields }
+
 [SOQLBuilder]::new().
-    AddSObjects({ $_ -match 'SBQQ__QuoteLine__c\b' }).
-    AddFields({ $_ }).
-    AddParentQueries([SOQLBuilder]::new().
-        AddSObjects({ $_ -eq 'SBQQ__Quote__c' }).
-        AddFields({ $_ }).
-        AddParentQueries([SOQLBuilder]::new().
-            AddSObjects({ $_ -eq 'Opportunity' }).
-            AddFields({ $_ }))).
-    AddWhere("SBQQ__ChargeType__c = 'Recurring' AND SBQQ__ProductCode__c LIKE 'SF_%'").
-    AddOrderBy({ $_.name -eq 'CreatedDate' }, 'DESC').
-    ExportQueries('Recurring')
-
-
-# Get fieldInfo sample obj
-# $(sf sobject describe -s blng__InvoiceLine__c --json | ConvertFrom-Json).result.fields[0]
-
-# Get childRelationship sample obj
-# $(sf sobject describe -s blng__InvoiceLine__c --json | ConvertFrom-Json).result.childRelationships[0]
+    AddSObjects({ $_ -match 'financeperiod__c\b' }).
+    AddFields($FieldSelector).
+    AddOrderBy({$_.name -eq 'blng__PeriodEndDate__c'}).
+    Export().
+    Execute()
+    | Out-Null
